30ffa6753d616f0b4f3af3a9d2d6437436264043
Gabriel <gbns.tech@gmail.com>
2023-05-15 21:35:50 -0300

    2.1-criando-a-entidade-categoria

---
+++ domain/src/main/java/com/fullcycle/catalogo/domain/category/Category.java
@@ -0,0 +1,92 @@
+package com.fullcycle.catalogo.domain.category;
+
+import java.time.Instant;
+
+public class Category {
+
+    private final String id;
+    private final String name;
+    private final String description;
+    private final boolean active;
+    private final Instant createdAt;
+    private final Instant updatedAt;
+    private final Instant deletedAt;
+
+    private Category(
+            final String anId,
+            final String aName,
+            final String aDescription,
+            final boolean isActive,
+            final Instant aCreationDate,
+            final Instant aUpdateDate,
+            final Instant aDeleteDate
+    ) {
+        this.id = anId;
+        this.name = aName;
+        this.description = aDescription;
+        this.active = isActive;
+        this.createdAt = aCreationDate;
+        this.updatedAt = aUpdateDate;
+        this.deletedAt = aDeleteDate;
+    }
+
+    public static Category with(
+            final String anId,
+            final String name,
+            final String description,
+            final boolean active,
+            final Instant createdAt,
+            final Instant updatedAt,
+            final Instant deletedAt
+    ) {
+        return new Category(
+                anId,
+                name,
+                description,
+                active,
+                createdAt,
+                updatedAt,
+                deletedAt
+        );
+    }
+
+    public static Category with(final Category aCategory) {
+        return with(
+                aCategory.id(),
+                aCategory.name(),
+                aCategory.description(),
+                aCategory.active(),
+                aCategory.createdAt(),
+                aCategory.updatedAt(),
+                aCategory.deletedAt()
+        );
+    }
+
+    public String id() {
+        return id;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String description() {
+        return description;
+    }
+
+    public boolean active() {
+        return active;
+    }
+
+    public Instant createdAt() {
+        return createdAt;
+    }
+
+    public Instant updatedAt() {
+        return updatedAt;
+    }
+
+    public Instant deletedAt() {
+        return deletedAt;
+    }
+}
