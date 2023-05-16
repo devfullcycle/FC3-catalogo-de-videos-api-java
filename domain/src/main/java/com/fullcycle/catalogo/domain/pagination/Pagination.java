387f3ee24ae9b6145ca025495fabdad99e04a602
Gabriel <gbns.tech@gmail.com>
2023-05-15 21:56:26 -0300

    2.4-criando-a-interface-do-gateway-de-categoria

---
+++ domain/src/main/java/com/fullcycle/catalogo/domain/pagination/Pagination.java
@@ -0,0 +1,20 @@
+package com.fullcycle.catalogo.domain.pagination;
+
+import java.util.List;
+import java.util.function.Function;
+
+public record Pagination<T>(
+        int currentPage,
+        int perPage,
+        long total,
+        List<T> items
+) {
+
+    public <R> Pagination<R> map(final Function<T, R> mapper) {
+        final List<R> aNewList = this.items.stream()
+                .map(mapper)
+                .toList();
+
+        return new Pagination<>(currentPage(), perPage(), total(), aNewList);
+    }
+}
