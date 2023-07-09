74728ea77d465a7e89cb1a8727745990f93054a5
Gabriel <gbns.tech@gmail.com>
2023-07-09 19:35:50 -0300

    5.04-testes-integrados-do-metodo-save-do-gateway-de-categoria

--- infrastructure/src/test/java/com/fullcycle/catalogo/infrastructure/category/CategoryElasticsearchGatewayTest.java
+++ infrastructure/src/test/java/com/fullcycle/catalogo/infrastructure/category/CategoryElasticsearchGatewayTest.java
@@ -1,6 +1,7 @@
 package com.fullcycle.catalogo.infrastructure.category;
 
 import com.fullcycle.catalogo.AbstractElasticsearchTest;
+import com.fullcycle.catalogo.domain.Fixture;
 import com.fullcycle.catalogo.infrastructure.category.persistence.CategoryRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,10 +10,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class CategoryElasticsearchGatewayTest extends AbstractElasticsearchTest {
 
     @Autowired
+    private CategoryElasticsearchGateway categoryGateway;
+
+    @Autowired
     private CategoryRepository categoryRepository;
 
     @Test
     public void testInjection() {
+        Assertions.assertNotNull(categoryGateway);
         Assertions.assertNotNull(categoryRepository);
+    }
+
+    @Test
+    public void givenValidCategory_whenCallsSave_shouldPersistIt() {
+        // given
+        final var aulas = Fixture.Categories.aulas();
+
+        // when
+        final var actualOutput = this.categoryGateway.save(aulas);
+
+        // then
+        Assertions.assertEquals(aulas, actualOutput);
+
+        final var actualCategory = categoryRepository.findById(aulas.id()).get();
+        Assertions.assertEquals(aulas.id(), actualCategory.id());
+        Assertions.assertEquals(aulas.name(), actualCategory.name());
+        Assertions.assertEquals(aulas.description(), actualCategory.description());
+        Assertions.assertEquals(aulas.active(), actualCategory.active());
+        Assertions.assertEquals(aulas.createdAt(), actualCategory.createdAt());
+        Assertions.assertEquals(aulas.updatedAt(), actualCategory.updatedAt());
+        Assertions.assertEquals(aulas.deletedAt(), actualCategory.deletedAt());
     }
 }
