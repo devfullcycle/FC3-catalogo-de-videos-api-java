752cd57a3e1229dac93d6e124c33f92db9c5bd05
Gabriel <gbns.tech@gmail.com>
2023-07-09 19:27:41 -0300

    5.03-criando-a-implementacao-do-gateway-de-categoria

---
+++ infrastructure/src/main/java/com/fullcycle/catalogo/infrastructure/category/CategoryElasticsearchGateway.java
@@ -0,0 +1,47 @@
+package com.fullcycle.catalogo.infrastructure.category;
+
+import com.fullcycle.catalogo.domain.category.Category;
+import com.fullcycle.catalogo.domain.category.CategoryGateway;
+import com.fullcycle.catalogo.domain.category.CategorySearchQuery;
+import com.fullcycle.catalogo.domain.pagination.Pagination;
+import com.fullcycle.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.springframework.data.elasticsearch.core.SearchOperations;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@Component
+public class CategoryElasticsearchGateway implements CategoryGateway {
+
+    private final CategoryRepository categoryRepository;
+    private final SearchOperations searchOperations;
+
+    public CategoryElasticsearchGateway(
+            final CategoryRepository categoryRepository,
+            final SearchOperations searchOperations
+    ) {
+        this.categoryRepository = Objects.requireNonNull(categoryRepository);
+        this.searchOperations = Objects.requireNonNull(searchOperations);
+    }
+
+    @Override
+    public Category save(Category aCategory) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void deleteById(String anId) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Optional<Category> findById(String anId) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Pagination<Category> findAll(CategorySearchQuery aQuery) {
+        throw new UnsupportedOperationException();
+    }
+}
