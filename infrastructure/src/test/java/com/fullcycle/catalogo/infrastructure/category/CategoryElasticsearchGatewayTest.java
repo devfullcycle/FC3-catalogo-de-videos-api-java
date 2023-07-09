2a55aa097d495d97c447fd76328f195c9b8504f7
Gabriel <gbns.tech@gmail.com>
2023-07-09 19:50:19 -0300

    5.06-testes-integrados-do-metodo-delete-do-gateway-de-categoria

--- infrastructure/src/test/java/com/fullcycle/catalogo/infrastructure/category/CategoryElasticsearchGatewayTest.java
+++ infrastructure/src/test/java/com/fullcycle/catalogo/infrastructure/category/CategoryElasticsearchGatewayTest.java
@@ -2,6 +2,7 @@ package com.fullcycle.catalogo.infrastructure.category;
 
 import com.fullcycle.catalogo.AbstractElasticsearchTest;
 import com.fullcycle.catalogo.domain.Fixture;
+import com.fullcycle.catalogo.infrastructure.category.persistence.CategoryDocument;
 import com.fullcycle.catalogo.infrastructure.category.persistence.CategoryRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -17,8 +18,8 @@ public class CategoryElasticsearchGatewayTest extends AbstractElasticsearchTest 
 
     @Test
     public void testInjection() {
-        Assertions.assertNotNull(categoryGateway);
-        Assertions.assertNotNull(categoryRepository);
+        Assertions.assertNotNull(this.categoryGateway);
+        Assertions.assertNotNull(this.categoryRepository);
     }
 
     @Test
@@ -32,7 +33,7 @@ public class CategoryElasticsearchGatewayTest extends AbstractElasticsearchTest 
         // then
         Assertions.assertEquals(aulas, actualOutput);
 
-        final var actualCategory = categoryRepository.findById(aulas.id()).get();
+        final var actualCategory = this.categoryRepository.findById(aulas.id()).get();
         Assertions.assertEquals(aulas.id(), actualCategory.id());
         Assertions.assertEquals(aulas.name(), actualCategory.name());
         Assertions.assertEquals(aulas.description(), actualCategory.description());
@@ -40,5 +41,31 @@ public class CategoryElasticsearchGatewayTest extends AbstractElasticsearchTest 
         Assertions.assertEquals(aulas.createdAt(), actualCategory.createdAt());
         Assertions.assertEquals(aulas.updatedAt(), actualCategory.updatedAt());
         Assertions.assertEquals(aulas.deletedAt(), actualCategory.deletedAt());
+    }
+
+    @Test
+    public void givenValidId_whenCallsDeleteById_shouldDeleteIt() {
+        // given
+        final var aulas = Fixture.Categories.aulas();
+
+        this.categoryRepository.save(CategoryDocument.from(aulas));
+
+        final var expectedId = aulas.id();
+        Assertions.assertTrue(this.categoryRepository.existsById(expectedId));
+
+        // when
+        this.categoryGateway.deleteById(expectedId);
+
+        // then
+        Assertions.assertFalse(this.categoryRepository.existsById(expectedId));
+    }
+
+    @Test
+    public void givenInvalidId_whenCallsDeleteById_shouldBeOk() {
+        // given
+        final var expectedId = "any";
+
+        // when/then
+        Assertions.assertDoesNotThrow(() -> this.categoryGateway.deleteById(expectedId));
     }
 }
