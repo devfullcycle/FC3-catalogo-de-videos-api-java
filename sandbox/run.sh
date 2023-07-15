8083f17a480e5fd9e57415ffa131e8ff7d396f9d
Gabriel <gbns.tech@gmail.com>
2023-07-15 17:40:05 -0300

    6.05-rodando-kafka-connect-com-docker

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -5,7 +5,9 @@ docker network create kafka
 # Criar os docker volumes
 docker volume create es01
 docker volume create kafka01
+docker volume create kconnect01
 
 # Criar as pastas com permissões
 docker compose -f elk/docker-compose.yml up -d elasticsearch
 docker compose -f kafka/docker-compose.yml up -d
+docker compose -f catalogo_services/docker-compose.yml up -d
