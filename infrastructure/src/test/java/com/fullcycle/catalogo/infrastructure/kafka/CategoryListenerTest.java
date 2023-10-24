f60537c7b17ef8ff2d2fdc578c82e419883237ad
Gabriel <gbns.tech@gmail.com>
2023-10-23 21:11:24 -0300

    7.10-mais-testes-integrados-do-kafka-listener-de-categoria

--- infrastructure/src/test/java/com/fullcycle/catalogo/infrastructure/kafka/CategoryListenerTest.java
+++ infrastructure/src/test/java/com/fullcycle/catalogo/infrastructure/kafka/CategoryListenerTest.java
@@ -10,6 +10,7 @@ import com.fullcycle.catalogo.infrastructure.configuration.json.Json;
 import com.fullcycle.catalogo.infrastructure.kafka.models.connect.MessageValue;
 import com.fullcycle.catalogo.infrastructure.kafka.models.connect.Operation;
 import com.fullcycle.catalogo.infrastructure.kafka.models.connect.ValuePayload;
+import org.apache.kafka.clients.admin.TopicListing;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -20,8 +21,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.kafka.listener.adapter.ConsumerRecordMetadata;
 
+import java.util.Optional;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -47,8 +50,38 @@ public class CategoryListenerTest extends AbstractEmbeddedKafkaTest {
     private ArgumentCaptor<ConsumerRecordMetadata> metadata;
 
     @Test
+    public void testCategoriesTopics() throws Exception {
+        // given
+        final var expectedMainTopic = "adm_videos_mysql.adm_videos.categories";
+        final var expectedRetry0Topic = "adm_videos_mysql.adm_videos.categories-retry-0";
+        final var expectedRetry1Topic = "adm_videos_mysql.adm_videos.categories-retry-1";
+        final var expectedRetry2Topic = "adm_videos_mysql.adm_videos.categories-retry-2";
+        final var expectedDLTTopic = "adm_videos_mysql.adm_videos.categories-dlt";
+
+        // when
+        final var actualTopics = admin().listTopics().listings().get().stream()
+                .map(TopicListing::name)
+                .collect(Collectors.toSet());
+
+        // then
+        Assertions.assertTrue(actualTopics.contains(expectedMainTopic));
+        Assertions.assertTrue(actualTopics.contains(expectedRetry0Topic));
+        Assertions.assertTrue(actualTopics.contains(expectedRetry1Topic));
+        Assertions.assertTrue(actualTopics.contains(expectedRetry2Topic));
+        Assertions.assertTrue(actualTopics.contains(expectedDLTTopic));
+    }
+
+    @Test
     public void givenInvalidResponsesFromHandlerShouldRetryUntilGoesToDLT() throws Exception {
         // given
+        final var expectedMaxAttempts = 4;
+        final var expectedMaxDLTAttempts = 1;
+        final var expectedMainTopic = "adm_videos_mysql.adm_videos.categories";
+        final var expectedRetry0Topic = "adm_videos_mysql.adm_videos.categories-retry-0";
+        final var expectedRetry1Topic = "adm_videos_mysql.adm_videos.categories-retry-1";
+        final var expectedRetry2Topic = "adm_videos_mysql.adm_videos.categories-retry-2";
+        final var expectedDLTTopic = "adm_videos_mysql.adm_videos.categories-dlt";
+
         final var aulas = Fixture.Categories.aulas();
         final var aulasEvent = new CategoryEvent(aulas.id());
 
@@ -72,16 +105,102 @@ public class CategoryListenerTest extends AbstractEmbeddedKafkaTest {
         Assertions.assertTrue(latch.await(1, TimeUnit.MINUTES));
 
         // then
-        verify(categoryListener, times(4)).onMessage(eq(message), metadata.capture());
+        verify(categoryListener, times(expectedMaxAttempts)).onMessage(eq(message), metadata.capture());
 
         final var allMetas = metadata.getAllValues();
-        Assertions.assertEquals("adm_videos_mysql.adm_videos.categories", allMetas.get(0).topic());
-        Assertions.assertEquals("adm_videos_mysql.adm_videos.categories-retry-0", allMetas.get(1).topic());
-        Assertions.assertEquals("adm_videos_mysql.adm_videos.categories-retry-1", allMetas.get(2).topic());
-        Assertions.assertEquals("adm_videos_mysql.adm_videos.categories-retry-2", allMetas.get(3).topic());
+        Assertions.assertEquals(expectedMainTopic, allMetas.get(0).topic());
+        Assertions.assertEquals(expectedRetry0Topic, allMetas.get(1).topic());
+        Assertions.assertEquals(expectedRetry1Topic, allMetas.get(2).topic());
+        Assertions.assertEquals(expectedRetry2Topic, allMetas.get(3).topic());
 
-        verify(categoryListener, times(1)).onDLTMessage(eq(message), metadata.capture());
+        verify(categoryListener, times(expectedMaxDLTAttempts)).onDLTMessage(eq(message), metadata.capture());
 
-        Assertions.assertEquals("adm_videos_mysql.adm_videos.categories-dlt", metadata.getValue().topic());
+        Assertions.assertEquals(expectedDLTTopic, metadata.getValue().topic());
+    }
+
+    @Test
+    public void givenUpdateOperationWhenProcessGoesOKShouldEndTheOperation() throws Exception {
+        // given
+        final var aulas = Fixture.Categories.aulas();
+        final var aulasEvent = new CategoryEvent(aulas.id());
+
+        final var message =
+                Json.writeValueAsString(new MessageValue<>(new ValuePayload<>(aulasEvent, aulasEvent, aSource(), Operation.UPDATE)));
+
+        final var latch = new CountDownLatch(1);
+
+        doAnswer(t -> {
+            latch.countDown();
+            return aulas;
+        }).when(saveCategoryUseCase).execute(any());
+
+        doReturn(Optional.of(aulas)).when(categoryGateway).categoryOfId(any());
+
+        // when
+        producer().send(new ProducerRecord<>(categoryTopic, message));
+        producer().flush();
+
+        Assertions.assertTrue(latch.await(1, TimeUnit.MINUTES));
+
+        // then
+        verify(categoryGateway, times(1)).categoryOfId(eq(aulas.id()));
+
+        verify(saveCategoryUseCase, times(1)).execute(eq(aulas));
+    }
+
+    @Test
+    public void givenCreateOperationWhenProcessGoesOKShouldEndTheOperation() throws Exception {
+        // given
+        final var aulas = Fixture.Categories.aulas();
+        final var aulasEvent = new CategoryEvent(aulas.id());
+
+        final var message =
+                Json.writeValueAsString(new MessageValue<>(new ValuePayload<>(aulasEvent, aulasEvent, aSource(), Operation.CREATE)));
+
+        final var latch = new CountDownLatch(1);
+
+        doAnswer(t -> {
+            latch.countDown();
+            return aulas;
+        }).when(saveCategoryUseCase).execute(any());
+
+        doReturn(Optional.of(aulas)).when(categoryGateway).categoryOfId(any());
+
+        // when
+        producer().send(new ProducerRecord<>(categoryTopic, message));
+        producer().flush();
+
+        Assertions.assertTrue(latch.await(1, TimeUnit.MINUTES));
+
+        // then
+        verify(categoryGateway, times(1)).categoryOfId(eq(aulas.id()));
+
+        verify(saveCategoryUseCase, times(1)).execute(eq(aulas));
+    }
+
+    @Test
+    public void givenDeleteOperationWhenProcessGoesOKShouldEndTheOperation() throws Exception {
+        // given
+        final var aulas = Fixture.Categories.aulas();
+        final var aulasEvent = new CategoryEvent(aulas.id());
+
+        final var message =
+                Json.writeValueAsString(new MessageValue<>(new ValuePayload<>(aulasEvent, aulasEvent, aSource(), Operation.DELETE)));
+
+        final var latch = new CountDownLatch(1);
+
+        doAnswer(t -> {
+            latch.countDown();
+            return null;
+        }).when(deleteCategoryUseCase).execute(any());
+
+        // when
+        producer().send(new ProducerRecord<>(categoryTopic, message));
+        producer().flush();
+
+        Assertions.assertTrue(latch.await(1, TimeUnit.MINUTES));
+
+        // then
+        verify(deleteCategoryUseCase, times(1)).execute(eq(aulas.id()));
     }
 }
