0793f456e83928940f1e0ecaeae25628d50e4257
Gabriel <gbns.tech@gmail.com>
2024-02-04 13:49:27 -0300

    09.03-criando-a-interface-do-gateway-de-cast-member

---
+++ domain/src/main/java/com/fullcycle/catalogo/domain/castmember/CastMemberGateway.java
@@ -0,0 +1,16 @@
+package com.fullcycle.catalogo.domain.castmember;
+
+import com.fullcycle.catalogo.domain.pagination.Pagination;
+
+import java.util.Optional;
+
+public interface CastMemberGateway {
+
+    CastMember save(CastMember aMember);
+
+    void deleteById(String anId);
+
+    Optional<CastMember> findById(String anId);
+
+    Pagination<CastMember> findAll(CastMemberSearchQuery aQuery);
+}
