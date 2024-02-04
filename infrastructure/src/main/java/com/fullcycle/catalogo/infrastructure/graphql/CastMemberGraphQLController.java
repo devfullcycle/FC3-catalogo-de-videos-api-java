29414abefc814cf3a43dff019ea3abb4aa5bc893
Gabriel <gbns.tech@gmail.com>
2024-02-04 15:28:01 -0300

    10.03-tdd-e-implementacao-da-mutation-de-cast-member

--- infrastructure/src/main/java/com/fullcycle/catalogo/infrastructure/graphql/CastMemberGraphQLController.java
+++ infrastructure/src/main/java/com/fullcycle/catalogo/infrastructure/graphql/CastMemberGraphQLController.java
@@ -2,8 +2,12 @@ package com.fullcycle.catalogo.infrastructure.graphql;
 
 import com.fullcycle.catalogo.application.castmember.list.ListCastMemberUseCase;
 import com.fullcycle.catalogo.application.castmember.list.ListCastMembersOutput;
+import com.fullcycle.catalogo.application.castmember.save.SaveCastMemberUseCase;
+import com.fullcycle.catalogo.domain.castmember.CastMember;
 import com.fullcycle.catalogo.domain.castmember.CastMemberSearchQuery;
+import com.fullcycle.catalogo.infrastructure.castmember.models.CastMemberDTO;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -14,9 +18,14 @@ import java.util.Objects;
 public class CastMemberGraphQLController {
 
     private final ListCastMemberUseCase listCastMemberUseCase;
+    private final SaveCastMemberUseCase saveCastMemberUseCase;
 
-    public CastMemberGraphQLController(final ListCastMemberUseCase listCastMemberUseCase) {
+    public CastMemberGraphQLController(
+            final ListCastMemberUseCase listCastMemberUseCase,
+            final SaveCastMemberUseCase saveCastMemberUseCase
+    ) {
         this.listCastMemberUseCase = Objects.requireNonNull(listCastMemberUseCase);
+        this.saveCastMemberUseCase = Objects.requireNonNull(saveCastMemberUseCase);
     }
 
     @QueryMapping
@@ -29,7 +38,12 @@ public class CastMemberGraphQLController {
     ) {
         final var query =
                 new CastMemberSearchQuery(page, perPage, search, sort, direction);
-        
+
         return this.listCastMemberUseCase.execute(query).data();
+    }
+
+    @MutationMapping
+    public CastMember saveCastMember(@Argument CastMemberDTO input) {
+        return this.saveCastMemberUseCase.execute(input.toCastMember());
     }
 }
--- infrastructure/src/test/java/com/fullcycle/catalogo/infrastructure/graphql/CastMemberGraphQLControllerTest.java
+++ infrastructure/src/test/java/com/fullcycle/catalogo/infrastructure/graphql/CastMemberGraphQLControllerTest.java
@@ -3,9 +3,14 @@ package com.fullcycle.catalogo.infrastructure.graphql;
 import com.fullcycle.catalogo.GraphQLControllerTest;
 import com.fullcycle.catalogo.application.castmember.list.ListCastMemberUseCase;
 import com.fullcycle.catalogo.application.castmember.list.ListCastMembersOutput;
+import com.fullcycle.catalogo.application.castmember.save.SaveCastMemberUseCase;
 import com.fullcycle.catalogo.domain.Fixture;
+import com.fullcycle.catalogo.domain.castmember.CastMember;
 import com.fullcycle.catalogo.domain.castmember.CastMemberSearchQuery;
+import com.fullcycle.catalogo.domain.castmember.CastMemberType;
 import com.fullcycle.catalogo.domain.pagination.Pagination;
+import com.fullcycle.catalogo.domain.utils.IdUtils;
+import com.fullcycle.catalogo.domain.utils.InstantUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
@@ -13,8 +18,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.graphql.test.tester.GraphQlTester;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.Map;
 
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -23,6 +31,9 @@ public class CastMemberGraphQLControllerTest {
 
     @MockBean
     private ListCastMemberUseCase listCastMemberUseCase;
+
+    @MockBean
+    private SaveCastMemberUseCase saveCastMemberUseCase;
 
     @Autowired
     private GraphQlTester graphql;
@@ -140,5 +151,59 @@ public class CastMemberGraphQLControllerTest {
         Assertions.assertEquals(expectedSort, actualQuery.sort());
         Assertions.assertEquals(expectedDirection, actualQuery.direction());
         Assertions.assertEquals(expectedSearch, actualQuery.terms());
+    }
+
+    @Test
+    public void givenCastMemberInputWhenCallsSaveCastMemberMutationShouldPersistAndReturn() {
+        // given
+        final var expectedId = IdUtils.uniqueId();
+        final var expectedName = "Gabriel";
+        final var expectedType = CastMemberType.ACTOR;
+        final var expectedCreatedAt = InstantUtils.now();
+        final var expectedUpdatedAt = InstantUtils.now();
+
+        final var input = Map.of(
+                "id", expectedId,
+                "name", expectedName,
+                "type", expectedType.name(),
+                "createdAt", expectedCreatedAt.toString(),
+                "updatedAt", expectedUpdatedAt.toString()
+        );
+
+        final var query = """
+                mutation SaveCastMember($input: CastMemberInput!) {
+                    castMember: saveCastMember(input: $input) {
+                        id
+                        name
+                        type
+                        createdAt
+                        updatedAt
+                    }
+                }
+                """;
+
+        doAnswer(returnsFirstArg()).when(saveCastMemberUseCase).execute(any());
+
+        // when
+        this.graphql.document(query)
+                .variable("input", input)
+                .execute()
+                .path("castMember.id").entity(String.class).isEqualTo(expectedId)
+                .path("castMember.name").entity(String.class).isEqualTo(expectedName)
+                .path("castMember.type").entity(CastMemberType.class).isEqualTo(expectedType)
+                .path("castMember.createdAt").entity(Instant.class).isEqualTo(expectedCreatedAt)
+                .path("castMember.updatedAt").entity(Instant.class).isEqualTo(expectedUpdatedAt);
+
+        // then
+        final var capturer = ArgumentCaptor.forClass(CastMember.class);
+
+        verify(this.saveCastMemberUseCase, times(1)).execute(capturer.capture());
+
+        final var actualCategory = capturer.getValue();
+        Assertions.assertEquals(expectedId, actualCategory.id());
+        Assertions.assertEquals(expectedName, actualCategory.name());
+        Assertions.assertEquals(expectedType, actualCategory.type());
+        Assertions.assertEquals(expectedCreatedAt, actualCategory.createdAt());
+        Assertions.assertEquals(expectedUpdatedAt, actualCategory.updatedAt());
     }
 }
