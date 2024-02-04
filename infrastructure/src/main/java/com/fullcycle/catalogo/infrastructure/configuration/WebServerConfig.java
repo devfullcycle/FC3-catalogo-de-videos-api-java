1c12494ac4f35fd79cbd101b34714f59c86c9113
Gabriel <gbns.tech@gmail.com>
2024-02-04 12:54:18 -0300

    8.15-teste-de-regressao-completo-entre-microservicos-com-alguns-fixes

--- infrastructure/src/main/java/com/fullcycle/catalogo/infrastructure/configuration/WebServerConfig.java
+++ infrastructure/src/main/java/com/fullcycle/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -1,11 +1,25 @@
 package com.fullcycle.catalogo.infrastructure.configuration;
 
+import com.fullcycle.catalogo.infrastructure.authentication.RefreshClientCredentials;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration(proxyBeanMethods = false)
 @ComponentScan("com.fullcycle.catalogo")
 @EnableScheduling
 public class WebServerConfig {
+
+
+    @Bean
+    @Profile("!test-integration && !test-e2e")
+    ApplicationListener<ContextRefreshedEvent> refreshClientCredentials(final RefreshClientCredentials refreshClientCredentials) {
+        return ev -> {
+            refreshClientCredentials.refresh();
+        };
+    }
 }
