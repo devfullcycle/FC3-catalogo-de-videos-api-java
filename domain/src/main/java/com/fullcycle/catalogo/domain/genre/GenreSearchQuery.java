aac4e84c201654ad1b32b2aa915341cd4c49b295
Gabriel <gbns.tech@gmail.com>
2024-02-12 14:58:52 -0300

    13.06-testes-do-caso-de-uso-de-listagem-de-genre

--- domain/src/main/java/com/fullcycle/catalogo/domain/genre/GenreSearchQuery.java
+++ domain/src/main/java/com/fullcycle/catalogo/domain/genre/GenreSearchQuery.java
@@ -1,10 +1,13 @@
 package com.fullcycle.catalogo.domain.genre;
 
+import java.util.Set;
+
 public record GenreSearchQuery(
         int page,
         int perPage,
         String terms,
         String sort,
-        String direction
+        String direction,
+        Set<String> categories
 ) {
 }
