0e72b40551de560b0724b55cc2f4022c6fc7b1eb
Gabriel <gbns.tech@gmail.com>
2024-02-14 13:52:43 -0300

    17.02-criando-a-entidade-video

---
+++ domain/src/main/java/com/fullcycle/catalogo/domain/video/Video.java
@@ -0,0 +1,209 @@
+package com.fullcycle.catalogo.domain.video;
+
+import java.time.Instant;
+import java.time.Year;
+import java.util.Set;
+
+public class Video {
+
+    private String id;
+    private String title;
+    private String description;
+    private Year launchedAt;
+    private double duration;
+    private Rating rating;
+
+    private boolean opened;
+    private boolean published;
+
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    private String banner;
+    private String thumbnail;
+    private String thumbnailHalf;
+
+    private String trailer;
+    private String video;
+
+    private Set<String> categories;
+    private Set<String> genres;
+    private Set<String> castMembers;
+
+    private Video(
+            final String id,
+            final String title,
+            final String description,
+            final Year launchedAt,
+            final double duration,
+            final Rating rating,
+            final boolean opened,
+            final boolean published,
+            final Instant createdAt,
+            final Instant updatedAt,
+            final String video,
+            final String trailer,
+            final String banner,
+            final String thumbnail,
+            final String thumbnailHalf,
+            final Set<String> categories,
+            final Set<String> castMembers,
+            final Set<String> genres
+    ) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.launchedAt = launchedAt;
+        this.duration = duration;
+        this.rating = rating;
+        this.opened = opened;
+        this.published = published;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.video = video;
+        this.trailer = trailer;
+        this.banner = banner;
+        this.thumbnail = thumbnail;
+        this.thumbnailHalf = thumbnailHalf;
+        this.categories = categories;
+        this.genres = genres;
+        this.castMembers = castMembers;
+    }
+
+    public static Video with(
+            final String id,
+            final String title,
+            final String description,
+            final Year launchedAt,
+            final double duration,
+            final Rating rating,
+            final boolean opened,
+            final boolean published,
+            final Instant createdAt,
+            final Instant updatedAt,
+            final String video,
+            final String trailer,
+            final String banner,
+            final String thumbnail,
+            final String thumbnailHalf,
+            final Set<String> categories,
+            final Set<String> castMembers,
+            final Set<String> genres
+    ) {
+        return new Video(
+                id,
+                title,
+                description,
+                launchedAt,
+                duration,
+                rating,
+                opened,
+                published,
+                createdAt,
+                updatedAt,
+                video,
+                trailer,
+                banner,
+                thumbnail,
+                thumbnailHalf,
+                categories,
+                castMembers,
+                genres
+        );
+    }
+
+    public static Video with(final Video video) {
+        return with(
+                video.id(),
+                video.title(),
+                video.description(),
+                video.launchedAt(),
+                video.duration(),
+                video.rating(),
+                video.opened(),
+                video.published(),
+                video.createdAt(),
+                video.updatedAt(),
+                video.video(),
+                video.trailer(),
+                video.banner(),
+                video.thumbnail(),
+                video.thumbnailHalf(),
+                video.categories(),
+                video.castMembers(),
+                video.genres()
+        );
+    }
+
+    public String id() {
+        return id;
+    }
+
+    public String title() {
+        return title;
+    }
+
+    public String description() {
+        return description;
+    }
+
+    public Year launchedAt() {
+        return launchedAt;
+    }
+
+    public double duration() {
+        return duration;
+    }
+
+    public Rating rating() {
+        return rating;
+    }
+
+    public boolean opened() {
+        return opened;
+    }
+
+    public boolean published() {
+        return published;
+    }
+
+    public Instant createdAt() {
+        return createdAt;
+    }
+
+    public Instant updatedAt() {
+        return updatedAt;
+    }
+
+    public String banner() {
+        return banner;
+    }
+
+    public String thumbnail() {
+        return thumbnail;
+    }
+
+    public String thumbnailHalf() {
+        return thumbnailHalf;
+    }
+
+    public String trailer() {
+        return trailer;
+    }
+
+    public String video() {
+        return video;
+    }
+
+    public Set<String> categories() {
+        return categories;
+    }
+
+    public Set<String> genres() {
+        return genres;
+    }
+
+    public Set<String> castMembers() {
+        return castMembers;
+    }
+}
--- domain/src/test/java/com/fullcycle/catalogo/domain/video/VideoTest.java
+++ domain/src/test/java/com/fullcycle/catalogo/domain/video/VideoTest.java
@@ -30,8 +30,8 @@ public class VideoTest extends UnitTest {
         final var expectedCreatedAt = InstantUtils.now();
         final var expectedUpdatedAt = InstantUtils.now();
         final var expectedCategories = Set.of(IdUtils.uniqueId());
+        final var expectedCastMembers = Set.of(IdUtils.uniqueId());
         final var expectedGenres = Set.of(IdUtils.uniqueId());
-        final var expectedMembers = Set.of(IdUtils.uniqueId());
         final var expectedVideo = "http://video";
         final var expectedTrailer = "http://trailer";
         final var expectedBanner = "http://banner";
@@ -43,21 +43,21 @@ public class VideoTest extends UnitTest {
                 expectedId,
                 expectedTitle,
                 expectedDescription,
-                expectedCreatedAt,
-                expectedUpdatedAt,
                 expectedLaunchedAt,
                 expectedDuration,
+                expectedRating,
                 expectedOpened,
                 expectedPublished,
-                expectedRating,
-                expectedCategories,
-                expectedGenres,
-                expectedMembers,
+                expectedCreatedAt,
+                expectedUpdatedAt,
                 expectedVideo,
                 expectedTrailer,
                 expectedBanner,
                 expectedThumbnail,
-                expectedThumbnailHalf
+                expectedThumbnailHalf,
+                expectedCategories,
+                expectedCastMembers,
+                expectedGenres
         );
 
         // then
@@ -74,11 +74,84 @@ public class VideoTest extends UnitTest {
         Assertions.assertEquals(expectedRating, actualVideo.rating());
         Assertions.assertEquals(expectedCategories, actualVideo.categories());
         Assertions.assertEquals(expectedGenres, actualVideo.genres());
-        Assertions.assertEquals(expectedMembers, actualVideo.castMembers());
-        Assertions.assertTrue(actualVideo.video().isEmpty());
-        Assertions.assertTrue(actualVideo.trailer().isEmpty());
-        Assertions.assertTrue(actualVideo.banner().isEmpty());
-        Assertions.assertTrue(actualVideo.thumbnail().isEmpty());
-        Assertions.assertTrue(actualVideo.thumbnailHalf().isEmpty());
+        Assertions.assertEquals(expectedCastMembers, actualVideo.castMembers());
+        Assertions.assertEquals(expectedVideo, actualVideo.video());
+        Assertions.assertEquals(expectedTrailer, actualVideo.trailer());
+        Assertions.assertEquals(expectedBanner, actualVideo.banner());
+        Assertions.assertEquals(expectedThumbnail, actualVideo.thumbnail());
+        Assertions.assertEquals(expectedThumbnailHalf, actualVideo.thumbnailHalf());
+    }
+
+    @Test
+    public void givenValidVideo_whenCallsVideoWith_shouldInstantiate() {
+        // given
+        final var expectedId = IdUtils.uniqueId();
+        final var expectedTitle = "System Design Interviews";
+        final var expectedDescription = """
+                Disclaimer: o estudo de caso apresentado tem fins educacionais e representa nossas opiniões pessoais.
+                Esse vídeo faz parte da Imersão Full Stack && Full Cycle.
+                Para acessar todas as aulas, lives e desafios, acesse:
+                https://imersao.fullcycle.com.br/
+                """;
+        final var expectedLaunchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCreatedAt = InstantUtils.now();
+        final var expectedUpdatedAt = InstantUtils.now();
+        final var expectedCategories = Set.of(IdUtils.uniqueId());
+        final var expectedCastMembers = Set.of(IdUtils.uniqueId());
+        final var expectedGenres = Set.of(IdUtils.uniqueId());
+        final var expectedVideo = "http://video";
+        final var expectedTrailer = "http://trailer";
+        final var expectedBanner = "http://banner";
+        final var expectedThumbnail = "http://thumb";
+        final var expectedThumbnailHalf = "http://thumbhalf";
+
+        final var video = Video.with(
+                expectedId,
+                expectedTitle,
+                expectedDescription,
+                expectedLaunchedAt,
+                expectedDuration,
+                expectedRating,
+                expectedOpened,
+                expectedPublished,
+                expectedCreatedAt,
+                expectedUpdatedAt,
+                expectedVideo,
+                expectedTrailer,
+                expectedBanner,
+                expectedThumbnail,
+                expectedThumbnailHalf,
+                expectedCategories,
+                expectedCastMembers,
+                expectedGenres
+        );
+
+        // when
+        final var actualVideo = Video.with(video);
+
+        // then
+        Assertions.assertNotNull(actualVideo);
+        Assertions.assertEquals(expectedId, actualVideo.id());
+        Assertions.assertEquals(expectedCreatedAt, actualVideo.createdAt());
+        Assertions.assertEquals(expectedUpdatedAt, actualVideo.updatedAt());
+        Assertions.assertEquals(expectedTitle, actualVideo.title());
+        Assertions.assertEquals(expectedDescription, actualVideo.description());
+        Assertions.assertEquals(expectedLaunchedAt, actualVideo.launchedAt());
+        Assertions.assertEquals(expectedDuration, actualVideo.duration());
+        Assertions.assertEquals(expectedOpened, actualVideo.opened());
+        Assertions.assertEquals(expectedPublished, actualVideo.published());
+        Assertions.assertEquals(expectedRating, actualVideo.rating());
+        Assertions.assertEquals(expectedCategories, actualVideo.categories());
+        Assertions.assertEquals(expectedGenres, actualVideo.genres());
+        Assertions.assertEquals(expectedCastMembers, actualVideo.castMembers());
+        Assertions.assertEquals(expectedVideo, actualVideo.video());
+        Assertions.assertEquals(expectedTrailer, actualVideo.trailer());
+        Assertions.assertEquals(expectedBanner, actualVideo.banner());
+        Assertions.assertEquals(expectedThumbnail, actualVideo.thumbnail());
+        Assertions.assertEquals(expectedThumbnailHalf, actualVideo.thumbnailHalf());
     }
 }
