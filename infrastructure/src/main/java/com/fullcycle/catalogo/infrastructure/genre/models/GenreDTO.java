93ba9d6629c6d2a395b922eb905d4861c9cfc014
Gabriel <gbns.tech@gmail.com>
2024-02-13 22:33:56 -0300

    16.03-testes-de-regressao-do-kafka-listener-de-genre

--- infrastructure/src/main/java/com/fullcycle/catalogo/infrastructure/genre/models/GenreDTO.java
+++ infrastructure/src/main/java/com/fullcycle/catalogo/infrastructure/genre/models/GenreDTO.java
@@ -9,8 +9,8 @@ import java.util.Set;
 public record GenreDTO(
         @JsonProperty("id") String id,
         @JsonProperty("name") String name,
-        @JsonProperty("active") Boolean active,
-        @JsonProperty("categories") Set<String> categories,
+        @JsonProperty("is_active") Boolean active,
+        @JsonProperty("categories_id") Set<String> categories,
         @JsonProperty("created_at") Instant createdAt,
         @JsonProperty("updated_at") Instant updatedAt,
         @JsonProperty("deleted_at") Instant deletedAt
