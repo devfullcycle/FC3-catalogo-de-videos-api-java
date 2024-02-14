32c5402f0c1ca263ee84e8a0ffc2f687d35f2506
Gabriel <gbns.tech@gmail.com>
2024-02-14 14:51:13 -0300

    17.08-configurando-os-beans-dos-usecases-de-video

---
+++ infrastructure/src/main/java/com/fullcycle/catalogo/infrastructure/configuration/usecases/VideoUseCasesConfig.java
@@ -0,0 +1,35 @@
+package com.fullcycle.catalogo.infrastructure.configuration.usecases;
+
+import com.fullcycle.catalogo.application.video.delete.DeleteVideoUseCase;
+import com.fullcycle.catalogo.application.video.list.ListVideoUseCase;
+import com.fullcycle.catalogo.application.video.save.SaveVideoUseCase;
+import com.fullcycle.catalogo.domain.video.VideoGateway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
+
+@Configuration(proxyBeanMethods = false)
+public class VideoUseCasesConfig {
+
+    private final VideoGateway videoGateway;
+
+    public VideoUseCasesConfig(final VideoGateway videoGateway) {
+        this.videoGateway = Objects.requireNonNull(videoGateway);
+    }
+
+    @Bean
+    DeleteVideoUseCase deleteVideoUseCase() {
+        return new DeleteVideoUseCase(videoGateway);
+    }
+
+    @Bean
+    ListVideoUseCase listVideoUseCase() {
+        return new ListVideoUseCase(videoGateway);
+    }
+
+    @Bean
+    SaveVideoUseCase saveVideoUseCase() {
+        return new SaveVideoUseCase(videoGateway);
+    }
+}
