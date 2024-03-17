6ea0ff9ee81c3003af59b5d7176cfd3b757fc7a6
Gabriel <gbns.tech@gmail.com>
2024-03-17 15:02:26 -0300

    19.03-criando-a-implementacao-do-gateway-de-video

--- infrastructure/src/main/java/com/fullcycle/catalogo/infrastructure/video/VideoElasticsearchGateway.java
+++ infrastructure/src/main/java/com/fullcycle/catalogo/infrastructure/video/VideoElasticsearchGateway.java
@@ -4,41 +4,43 @@ import com.fullcycle.catalogo.domain.pagination.Pagination;
 import com.fullcycle.catalogo.domain.video.Video;
 import com.fullcycle.catalogo.domain.video.VideoGateway;
 import com.fullcycle.catalogo.domain.video.VideoSearchQuery;
+import com.fullcycle.catalogo.infrastructure.video.persistence.VideoRepository;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.elasticsearch.core.SearchOperations;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Component
-@Profile("development")
-public class VideoInMemoryGateway implements VideoGateway {
+@Profile("!development")
+public class VideoElasticsearchGateway implements VideoGateway {
 
-    private final Map<String, Video> db;
+    private final SearchOperations searchOperations;
+    private final VideoRepository videoRepository;
 
-    public VideoInMemoryGateway() {
-        this.db = new ConcurrentHashMap<>();
+    public VideoElasticsearchGateway(final SearchOperations searchOperations, final VideoRepository videoRepository) {
+        this.searchOperations = Objects.requireNonNull(searchOperations);
+        this.videoRepository = Objects.requireNonNull(videoRepository);
     }
 
     @Override
     public Video save(Video video) {
-        this.db.put(video.id(), video);
-        return video;
+        return null;
     }
 
     @Override
     public void deleteById(String videoId) {
-        this.db.remove(videoId);
+
     }
 
     @Override
     public Optional<Video> findById(String videoId) {
-        return Optional.ofNullable(this.db.get(videoId));
+        return Optional.empty();
     }
 
     @Override
     public Pagination<Video> findAll(VideoSearchQuery aQuery) {
-        return new Pagination<>(aQuery.page(), aQuery.perPage(), this.db.size(), this.db.values().stream().toList());
+        return null;
     }
 }
